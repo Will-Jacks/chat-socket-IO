1411a2e38ab0d78764d4f19b39322ca197e1650d
William Jackson <lila.lilajackson@hotmail.com>
2023-06-05 21:59:32 -0300

    Funcionando, mas nada estético

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
 const porta = process.env.PORT || 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('public'));
+
 app.get('/', (req,res)=> {
     res.sendFile('C:/Users/lilal/Documents/Programação/my-chat-websocket/index.html');
 });
-app.listen(porta, console.log(`Escutando na porta: ${porta}`));
+
+io.on('connection', (socket) => {
+    socket.on('chat message', msg => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(porta, console.log(`Escutando na porta: ${porta}`));  
